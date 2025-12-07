b216aa3b79c34d124e15572e358fa647d5cd0607
PauSGracia <pau.sole@gracia.lasalle.cat>
2025-12-07 16:15:34 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+show_graph = True
+x = 2
+y = 2
 
+def on_forever():
+    global show_graph
+    if input.button_is_pressed(Button.A):
+        show_graph = True
+    elif input.button_is_pressed(Button.B):
+        show_graph = False  
+
+    if show_graph == True:
+        graph()
+    elif show_graph == False:
+        show_drop()
+    pass
+
+
+def graph():
+    led.plot_bar_graph(input.temperature(), 50)
+
+def show_drop():
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(500)
+    basic.clear_screen()
+
+    if accX <= 150 and x > 0:
+        x = x - 1
+    
+    if accX > 150 and y < 4:
+        x = x + 1
+
+    if accY <= 150 and y > 0:
+        y = y - 1
+
+    if accY > 150 and y < 4:
+        y = y + 1
+
+basic.forever(on_forever)
+    
--- main.ts
+++ main.ts
@@ -1,1 +1,49 @@
+let show_graph = true
+let x = 2
+let y = 2
+function graph() {
+    led.plotBarGraph(input.temperature(), 50)
+}
 
+function show_drop() {
+    let x: number;
+    let y: number;
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(500)
+    basic.clearScreen()
+    if (accX <= 150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accX > 150 && y < 4) {
+        x = x + 1
+    }
+    
+    if (accY <= 150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y = y + 1
+    }
+    
+}
+
+basic.forever(function on_forever() {
+    
+    if (input.buttonIsPressed(Button.A)) {
+        show_graph = true
+    } else if (input.buttonIsPressed(Button.B)) {
+        show_graph = false
+    }
+    
+    if (show_graph == true) {
+        graph()
+    } else if (show_graph == false) {
+        show_drop()
+    }
+    
+    
+})
